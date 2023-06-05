73ae35cb83622e1cc5479ea63d133a776d930871
DenverW03 <d.whiteley@lancaster.ac.uk>
2023-06-05 23:13:16 +0100

    removed unnecessary compiled files

--- ServoInterface/ServoInterface.ino
+++ ServoInterface/ServoInterface.ino
@@ -1,18 +1,24 @@
 #include <Servo.h>
 
-//Servo[] servos = Servo[11]; // uses pins 2 to 13 to communicate with servos
-
-Servo tendon;
-Servo ankle;
+Servo[] servos = Servo[11]; // uses pins 2 to 13 to communicate with servos
+// tendon 2, ankle 3
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  tendon.attach(2);
-  ankle.attach(3);
+  servos[0].attach(2);
+  servos[1].attach(3);
+  while(true){
+    if (Serial.available() > 0) {
+      String data = Serial.readString();
+      data.trim();
+      if(data.equals("start")){
+        break;
+      }
+    }
+  }
 }
 
 void loop() {
-  ankle.write(90);
   if(Serial.available() > 0){
     String data = Serial.readString();
     int numData = data.toInt();
