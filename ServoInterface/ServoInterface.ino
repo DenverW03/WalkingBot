c9046ad8a3cee433e5cf647b3401a0420da48ccb
DenverW03 <d.whiteley@lancaster.ac.uk>
2023-06-21 23:02:29 +0100

    working on version to work with ESP32 board

--- ServoInterface/ServoInterface.ino
+++ ServoInterface/ServoInterface.ino
@@ -1,12 +1,12 @@
-#include <Servo.h>
-
-Servo[] servos = Servo[11]; // uses pins 2 to 13 to communicate with servos
-// tendon 2, ankle 3
+#include <ESP32Servo.h>
+Servo backRight1, backRight2;
 void setup() {
-  // put your setup code here, to run once:
+  backRight1.attach(22);
+  backRight2.attach(23);
   Serial.begin(9600);
-  servos[0].attach(2);
-  servos[1].attach(3);
+  // starting positions
+  backRight1.write(90);
+  backRight2.write(60);
   while(true){
     if (Serial.available() > 0) {
       String data = Serial.readString();
@@ -21,9 +21,29 @@ void setup() {
 void loop() {
   if(Serial.available() > 0){
     String data = Serial.readString();
-    int numData = data.toInt();
-    Serial.println(numData);
-    tendon.write(numData);
-    delay(15);
+    data.trim();
+    char delimiter = ':';
+    char charArray[data.length() + 1];  // Create a character array
+    data.toCharArray(charArray, sizeof(charArray));  // Convert the string to a character array
+    char* token = strtok(charArray, &delimiter);  // Find the first token
+    char* firstPart;
+    char* secondPart;
+    if (token != NULL) {
+      firstPart = token;  // Store the first part
+      token = strtok(NULL, &delimiter);  // Find the second token
+      if (token != NULL) {
+        secondPart = token;  // Store the second part
+      }
+    }
+    moveServo(firstPart, atoi(secondPart));
+  }
+}
+
+void moveServo(char* name, int angle){
+  if(strcmp(name, "tendon") == 0){
+    backRight2.write(angle);
+  }
+  else if(strcmp(name, "ankle") == 0){
+    backRight1.write(angle);
   }
 }
